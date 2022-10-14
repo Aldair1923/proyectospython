1373c487af1295fc1de14475b5e8693cc6a88abf
Aldair1923 <115048524+Aldair1923@users.noreply.github.com>
2022-10-14 18:43:31 -0500

    Add files via upload

---
+++ convertirdewordapdf.py
@@ -0,0 +1,4 @@
+from docx2pdf import convert
+docx_file = 'documentox.docx' # poner el nombre del archivo a convertir
+pdf_file = 'archivox.pdf' # poner un nuevo nombre o...
+convert(docx_file, pdf_file)
